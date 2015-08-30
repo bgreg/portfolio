9c5ac749cc16cda95bab1f8f1b4e8395dbc144cb
Greg McGuirk <bert.mcguirk@gmail.com>
2015-08-30 11:44:22 -0700

    update tests

--- spec/lib/exposure_loader_spec.rb
+++ spec/lib/exposure_loader_spec.rb
@@ -2,15 +2,23 @@ require 'rails_helper'
 
 describe ExposureLoader do
   let(:remote_uri) { "http://static.nvd.nist.gov/feeds/xml/cve/nvdcve-2.0-modified.xml" }
-  let!(:nvd_data)  { File.read("spec/support/nvdcve-2.0-modified.xml") }
+  let!(:nvd_data)  { "spec/support/nvdcve-2.0-modified.xml" }
 
-  class StubLoader 
+  class StubLoader
     include ExposureLoader
   end
 
-  context "#download" do 
-    it "can download and import data" do 
-      expect( StubLoader.new.download(nvd_data)).to change(Exposure.count).by(300)
+  context "#download" do
+    before do
+      StubLoader.new.download(nvd_data)
+    end
+
+    it "will build exposures from an imported file" do
+      expect( Exposure.count ).to eq(554)
+    end
+
+    it "will build references fomr an imported file" do
+      expect( Reference.count ).to eq(1611)
     end
   end
 end
