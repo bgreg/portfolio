bbcfd8351eac3e2fb17e11d72a42f4d36513dd82
Greg McGuirk <bert.mcguirk@gmail.com>
2015-08-30 10:19:02 -0700

    tests passing

---
+++ db/migrate/20150830170753_create_exposures.rb
@@ -0,0 +1,23 @@
+class CreateExposures < ActiveRecord::Migration
+  def change
+    create_table :exposures do |t|
+      t.string :cve_id
+      t.text :summary
+      t.text :published
+      t.string :cvss_severity
+      t.text :title
+      t.integer :cvss_v2_base_score
+      t.text :access_vector
+      t.text :access_complexity
+      t.text :authentication
+      t.text :confidentiality_impact
+      t.text :integrity_impact
+      t.text :availablility_impact
+      t.boolean :ruby
+
+      t.timestamps
+    end
+    add_index(:exposures, :ruby)
+    add_index(:exposures, :cve_id)
+  end
+end
