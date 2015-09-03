705d5360c66a48f5979c69d917531d49d0349a01
Greg McGuirk <bert.mcguirk@gmail.com>
2015-09-02 21:01:24 -0700

    here have some code

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
-  resources :home
-  resources :blog
-  resources :news
+  resources :home, only: [:index]
+  resources :blog, only: [:index]
+  resources :news, only: [:index]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
